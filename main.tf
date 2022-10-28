ae803d10c307c485e7405e912d83f4747adbab99
jgomezve <jgomezve@cisco.com>
2022-10-28 12:05:57 -0500

    Create new Module

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "aci_rest_managed" "fvTenant" {
-  dn         = "uni/tn-${var.name}"
-  class_name = "fvTenant"
+resource "aci_rest_managed" "bgpBestPathCtrlPol" {
+  dn         = "uni/tn-${var.tenant}/bestpath-${var.name}"
+  class_name = "bgpBestPathCtrlPol"
   content = {
     name      = var.name
-    nameAlias = var.alias
     descr     = var.description
+    ctrl = var.control_type == "multi-path-relax" ? "asPathMultipathRelax" : ""
   }
 }
--- tests/minimal/test_minimal.tf
+++ tests/minimal/test_minimal.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.3.0"
 
   required_providers {
     test = {
@@ -13,36 +13,42 @@ terraform {
   }
 }
 
+resource "aci_rest_managed" "fvTenant" {
+  dn         = "uni/tn-TF"
+  class_name = "fvTenant"
+}
+
 module "main" {
   source = "../.."
 
-  name = "ABC"
+  name = "TEST_MINIMAL"
+  tenant = aci_rest_managed.fvTenant.content.name
 }
 
-data "aci_rest_managed" "fvTenant" {
-  dn = "uni/tn-ABC"
+data "aci_rest_managed" "bgpBestPathCtrlPol" {
+  dn = "uni/tn-${aci_rest_managed.fvTenant.content.name}/bestpath-TEST_MINIMAL"
 
   depends_on = [module.main]
 }
 
-resource "test_assertions" "fvTenant" {
-  component = "fvTenant"
+resource "test_assertions" "bgpBestPathCtrlPol" {
+  component = "bgpBestPathCtrlPol"
 
   equal "name" {
     description = "name"
-    got         = data.aci_rest_managed.fvTenant.content.name
-    want        = "ABC"
-  }
-
-  equal "nameAlias" {
-    description = "nameAlias"
-    got         = data.aci_rest_managed.fvTenant.content.nameAlias
-    want        = ""
+    got         = data.aci_rest_managed.bgpBestPathCtrlPol.content.name
+    want        = "TEST_MINIMAL"
   }
 
   equal "descr" {
     description = "descr"
-    got         = data.aci_rest_managed.fvTenant.content.descr
+    got         = data.aci_rest_managed.bgpBestPathCtrlPol.content.descr
+    want        = ""
+  }
+
+  equal "ctrl" {
+    description = "ctrl"
+    got         = data.aci_rest_managed.bgpBestPathCtrlPol.content.ctrl
     want        = ""
   }
 }
